ab6ae65a23ef220d31f87950fd09c7206a879ac1
73nici <30949443+73nici@users.noreply.github.com>
2019-01-10 18:28:45 +0100

    Deleted background.js

--- PluginDevelopment/Chrome/JS/options.js
+++ PluginDevelopment/Chrome/JS/options.js
@@ -1,29 +1,26 @@
-var ipAdress = '192.168.56.1'
-var port = "12345";
 document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById("button-save").addEventListener("click", sendsaveData);
 	document.getElementById("button-clear").addEventListener("click", clearData);
-	//console = chrome.extension.getBackgroundPage().console;
-	console.log("hello");
+
+	var ipAdress = '192.168.56.1'
+	var port = "12345";
+
 	function sendsaveData(){
-		//document.getElementById("button-save").innerHTML = "123";
 		var username = document.getElementById("inputUserName").value;
 		var password = document.getElementById("inputPassword").value;
-		//document.getElementById("button-save").innerHTML = password;
-
 		if(username == "" || password == "")
 			document.getElementById("inputUserName").value = "Es sind nicht alle Felder ausgefüllt";
 		else {
 			//get Username or set it for the first time
-			/*chrome.storage.sync.get(["username"], function(result){
-			 if(typeof result.username === "undefined")
+			chrome.storage.sync.get(["username"], function(result){
+			 if(typeof result.username === "undefined" || result.username == "")
 				chrome.storage.sync.set({username: username});
 			  });
 			//get password or set it for the first time
 			chrome.storage.sync.get(["password"], function(result){
-			  if(typeof result.password === "undefined")
-				chrome.storage.sync.set({password: password});
-			});*/
+			  if(typeof result.password === "undefined" || result.password == "")
+					chrome.storage.sync.set({password: password});
+			});
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", "http://"+ipAdress + ":" + port + "/users/create/", true);
 			xhr.onload = function(){
@@ -33,7 +30,6 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 
-
 	function clearData(){
 		chrome.storage.sync.set({username: ""});
 		chrome.storage.sync.set({password: ""});
@@ -42,5 +38,4 @@ document.addEventListener("DOMContentLoaded", function(){
 		document.getElementById("inputUserName").value = "";
 		document.getElementById("inputPassword").value = "";
 	}
-
 });
