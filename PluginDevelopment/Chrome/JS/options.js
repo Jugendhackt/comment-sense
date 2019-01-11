f241416725c7f42ecd3e00d43f84d37103e1b0da
73nici <30949443+73nici@users.noreply.github.com>
2019-01-11 18:54:42 +0100

    Save Password in options

--- PluginDevelopment/Chrome/JS/options.js
+++ PluginDevelopment/Chrome/JS/options.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById("button-save").addEventListener("click", sendsaveData);
-	document.getElementById("button-clear").addEventListener("click", clearData);
-
+	document.getElementById("button-savePassword").addEventListener("click", PasswordStatefnc);
 	var ipAdress = '192.168.56.1'
+	var PasswordState = false;
+
+	chrome.storage.sync.get(["PasswordState"], function(result){
+		if(typeof result.PasswordState === "undefined"){
+			chrome.storage.sync.set({PasswordState: PasswordState})
+		} else {
+			PasswordState = result.PasswordState;
+		}
+	});
+
+	checkPasswordvalue();
 
 	var error = document.getElementById("error");
 	var inputUsername = document.getElementById("inputUsername");
 	var inputPassword = document.getElementById("inputPassword");
+	var savePassword = document.getElementById("button-savePassword");
 
 	function sendsaveData(){
 		var username = inputUsername.value;
@@ -14,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function(){
 		if(username == "" || password == ""){
 			error.innerHTML = "Es sind nicht alle Felder ausgefüllt!";
 		} else {
-			error.innerHTML = "";
 
 			//get Username or set it for the first time
 			chrome.storage.sync.get(["username"], function(result){
@@ -36,9 +46,23 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 
-	function clearData(){
-		error.innerHTML = "";
-		inputUsername.value = "";
-		inputPassword.value = "";
+	function PasswordStatefnc() {
+		if(stateSavePassword == false) {
+			error.innerHTML = "Passwort wird gespeichert!";
+			stateSavePassword = true;
+			chrome.storage.sync.set({PasswordState: stateSavePassword});
+		} else {
+			error.innerHTML = "";
+			stateSavePassword = false;
+			chrome.storage.sync.set({PasswordState: stateSavePassword});
+		}
+	}
+
+	function checkPasswordvalue(){
+		chrome.storage.sync.get(["PasswordState"], function(result){
+			if(result.PasswordState == true){
+				window.location.href = "alloptions.html";
+			}
+		});
 	}
 });
