dffb0658590b0be0f67e33838ff7ada02b3dd6a5
73nici <30949443+73nici@users.noreply.github.com>
2019-01-11 17:05:11 +0100

    added CSS Code

--- PluginDevelopment/Chrome/JS/options.js
+++ PluginDevelopment/Chrome/JS/options.js
@@ -16,6 +16,17 @@ document.addEventListener("DOMContentLoaded", function(){
 		} else {
 			error.innerHTML = "";
 
+			//get Username or set it for the first time
+			chrome.storage.sync.get(["username"], function(result){
+			 if(typeof result.username === "undefined" || result.username == "")
+				chrome.storage.sync.set({username: username});
+			  });
+			//get password or set it for the first time
+			chrome.storage.sync.get(["password"], function(result){
+			  if(typeof result.password === "undefined" || result.password == "")
+					chrome.storage.sync.set({password: password});
+			});
+
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", "http://" + ipAdress + "/users/create/", true);
 			xhr.onload = function(){
