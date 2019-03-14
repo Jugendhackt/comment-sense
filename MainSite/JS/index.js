5dbd359d6f3154938f06438c4875383c6f256a35
Galaxyqasar <kunze.georg44@gmail.com>
2019-03-14 20:13:23 +0100

    Add files via upload

---
+++ MainSite/JS/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("createAcc").addEventListener("click", createAcc);
+    
+    function createAcc() {
+        window.location.href = "signup.html";
+    }
+});
