04e24acd09af314bb2fb4fc645b48d71edca5afd
73nici <30949443+73nici@users.noreply.github.com>
2019-03-23 01:10:37 +0100

    Fix

--- PluginDevelopment/Chrome/JS/login.js
+++ PluginDevelopment/Chrome/JS/login.js
@@ -1,24 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
+  if (window.location.href.split("/").pop() == "login.html")
+    document.getElementById("btnLogin").addEventListener("click", login);
+  const ipAdress = "192.168.2.113";
 
-  function getLoginData() {
+  const inputUsername = document.getElementById("inputUsername");
+  const inputPassword = document.getElementById("inputPassword");
+
+
+  function saveLoginData() {
     chrome.storage.sync.get(["save"], function(result) {
+      var username = inputUsername.value;
+      var password = inputPassword.value;
       if (result.save == true) {
-        chrome.storage.sync.get(["username", "password"], function(result) {
-          var xhr = new XMLHttpRequest();
-          xhr.open("GET", "http://" + ipAdress + "/users/login/", true);
-          xhr.onload = function() {
-            if (this.status !== 200){
-              window.location.href = "HTML/login.html";
-            }
-          }
-          xhr.send(JSON.stringify({
-            username: result.username,
-            password: result.password
-          }));
+        chrome.storage.sync.set({
+          username: username,
+          password: password
         });
       }
     });
   }
 
-  getLoginData();
+  function login() {
+    var username = inputUsername.value;
+    var password = inputPassword.value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://" + ipAdress + "/users/login/", true);
+    xhr.onload = function() {
+      var data = JSON.parse(this.responseText);
+      if (this.status === 200 && data.status == "login data valid")
+    }
+  }
+
+  function readLoginData() {
+    chrome.storage.sync.get(["username", "password", "save"], function(result) {
+      if (result.save == true) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://" + ipAdress + "/users/login/", true);
+        xhr.onload = function() {
+          var data = JSON.parse(this.responseText);
+          if (this.status !== 200 || data.status != "login not data valid") {
+            window.location.href = "HTML/login.html";
+          }
+        }
+        xhr.send(JSON.stringify({
+          userName: result.username,
+          password: result.password
+        }));
+      }
+    });
+  }
+
+
+
+  readLoginData();
 });
