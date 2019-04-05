8c97879987231bc47bd5adc5084a61703bbc452e
73nici <30949443+73nici@users.noreply.github.com>
2019-04-05 23:40:03 +0200

    new plugin

---
+++ Neues Plugin/JS/backend/backend.js
@@ -0,0 +1,95 @@
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("send").addEventListener("click", writeComment);
+	
+	
+	const ipAdress = "192.168.2.113";
+	const userName = "Test1";
+	const password = "123";
+
+
+	function refresh() {
+		chrome.tabs.query({
+			currentWindow: true,
+			active: true
+		}, function(tabs){
+			var url = tabs[0].url;
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", "http://" + ipAdress + "/comments/site='" + url + "'", true);			xhr.onload = function() {
+				if (this.status === 200 || this.status === 404) {
+					var data = JSON.parse(this.responseText);
+					//document.getElementById("comments").innerHTML = "";
+					for (var i = 0; i < data.Comments.length; i++) {
+						let userid = data.Comments[i].userId;
+						let username = data.Comments[i].userName;
+						let id = data.Comments[i].id;
+						let headline = data.Comments[i].headline;
+						let comment = data.Comments[i].content;
+						let votes = data.Comments[i].votes;
+						console.log(username);
+						displayComment(username, headline, comment, votes, id);
+
+					}
+				}
+			}
+			xhr.send();
+		});
+	}
+
+	function displayComment(author, headline, comment, liked, id) {
+		var div = document.createElement("DIV");
+		div.id = "comment" + id;
+		div.classList.add("comment");
+		document.getElementById("comments").appendChild(div);
+
+		var title = document.createElement("DIV");
+		title.innerHTML = headline;
+		title.classList.add("commentTitle");
+		div.appendChild(title);
+
+		var userName = document.createElement("DIV");
+		userName.innerHTML = author;
+		userName.classList.add("commentUser");
+		div.appendChild(userName);
+
+		var content = document.createElement("DIV");
+		content.innerHTML = comment;
+		content.classList.add("commentContent");
+		div.appendChild(content);
+
+	}
+
+	function writeComment() {
+		var comment = document.getElementById("inputContent").value;
+		var title = document.getElementById("inputTitle").value;
+
+		if (comment == "" || title == ""){
+			alert("Fehler!");
+		} else {
+			chrome.tabs.query({
+				currentWindow: true,
+				active: true
+			}, function(tabs) {
+				var url = tabs[0].url;
+				var xhr = new XMLHttpRequest();
+				xhr.open("POST", "http://" + ipAdress + "/comments/", true);
+				xhr.onload = function() {
+					console.log(this.responseText);
+					console.log(this.status);
+					if (this.status === 201)
+						alert("yeah");
+				}				
+				xhr.send(JSON.stringify({
+					userName: userName,
+					password: password,
+					headline: title,
+					comment: comment,
+					url: url
+				}));
+			});
+		}
+	}
+
+	refresh();
+
+
+});
---
+++ Neues Plugin/JS/options/options.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("login").addEventListener("click", loginUser);
+	
+	const ipAdress = "192.168.2.113";
+	
+	
+	function loginUser() {
+		var userName = document.getElementById("inputUserName").value;
+		var password = document.getElementById("inputPassword").value;
+		
+		var date = new Date();
+		date.setHours(1);
+		
+		if(userName == "" || password == "")
+			setErr("Passwort oder Nutzername leer!");
+		else {
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", "http://" + ipAdress + "/users/login/", true);
+			xhr.onload = function() {
+				//setErr(this.responseText);
+				var data = JSON.parse(this.responseText);
+				if (this.status === 200 && data.status == "login data valid") {
+					setErr("angemeldet");
+					
+					chrome.storage.local.set({"userName": userName, "password": password, "time":}, function(){
+						
+					});
+					
+					
+				} else if(this.status == "400") {
+					setErr("Nutzerdaten sind nicht vollständig!");
+				} else {
+					setErr(this.responseText);
+				}
+			}
+			xhr.send(JSON.stringify({
+				userName: userName,
+				password: password
+			}));
+		}
+		
+	}
+	
+	function setErr(err) {
+		document.getElementById("error").innerHTML += err;
+	}
+	
+});
