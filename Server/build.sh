a44b45f63cd304e285d215a1b859f72a7398f75f
Galaxyqasar <kunze.georg44@gmail.com>
2019-04-12 23:29:57 +0200

    Update build.sh

--- Server/build.sh
+++ Server/build.sh
@@ -2,5 +2,6 @@ gcc -c cJSON.c
 gcc -c socket.c
 gcc -c sqlite3.c
 gcc main.c cJSON.o socket.o sqlite3.o -o server -lpthread -ldl -Wall
+gcc test.c socket.o -o test -Wall
 mkdir data
 cp ../MainSite/* data/ -r
