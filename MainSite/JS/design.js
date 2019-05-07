b2b38dac58ce417dbbe2b71e18a84b98abc7516e
73nici <30949443+73nici@users.noreply.github.com>
2019-05-07 18:04:53 +0200

    setting icon changes wehen theme changes

--- MainSite/JS/design.js
+++ MainSite/JS/design.js
@@ -2,26 +2,30 @@ document.addEventListener("DOMContentLoaded", function() {
   function getDesign() {
     var design = localStorage.getItem("design");
     console.log(design);
-    if (typeof design == "undefined" || design == "light" || design == null)
+    if (typeof design == "undefined" || design == "light" || design == null) {
       loadjscssfile("CSS/design/light.css", "css");
-    else if (design == "dark")
+      loadjscssfile("assets/icons/settingLight512.png", "png");
+    } else if (design == "dark") {
       loadjscssfile("CSS/design/dark.css", "css");
+      loadjscssfile("assets/icons/settingDark512.png", "png");
+    }
   }
 
   function loadjscssfile(filename, filetype) {
-    if (filetype == "js") { //if filename is a external JavaScript file
-      // alert('called');
-      var fileref = document.createElement('script')
-      fileref.setAttribute("type", "text/javascript")
-      fileref.setAttribute("src", filename)
-    } else if (filetype == "css") { //if filename is an external CSS file
-      var fileref = document.createElement("link")
-      fileref.setAttribute("rel", "stylesheet")
-      fileref.setAttribute("type", "text/css")
-      fileref.setAttribute("href", filename)
+    if (filetype == "js") {
+      var fileref = document.createElement("script");
+      fileref.setAttribute("type", "text/javascript");
+      fileref.setAttribute("src", filename);
+    } else if (filetype == "css") {
+      var fileref = document.createElement("link");
+      fileref.setAttribute("rel", "stylesheet");
+      fileref.setAttribute("type", "text/css");
+      fileref.setAttribute("href", filename);
+    } else if (filetype == "png") {
+      document.getElementById("setting").src = filename;
     }
     if (typeof fileref != "undefined")
-      document.getElementsByTagName("head")[0].appendChild(fileref)
+      document.getElementsByTagName("head")[0].appendChild(fileref);
   }
 
   getDesign();
