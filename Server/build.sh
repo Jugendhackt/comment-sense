ee32f0adb234804c778072b16d9a764a2772ec9c
Galaxyqasar <kunze.georg44@gmail.com>
2019-05-25 22:38:15 +0200

    Update build.sh

--- Server/build.sh
+++ Server/build.sh
@@ -21,22 +21,57 @@ while [ "$1" != '' ]
 	shift
 done
 
-gcc -c cJSON.c
-gcc -c socket.c
-gcc -c sqlite3.c
-gcc -c string.c
-gcc -c httpHandler.c
-gcc -c commentSense.c
+if [ "cJSON.c" -nt "cJSON.o" ] || [ "cJSON.h" -nt "cJSON.o" ] 
+	then
+	echo "rebuilding cJSON"
+	gcc -c cJSON.c
+fi
+
+if [ "socket.c" -nt "socket.o" ] || [ "socket.h" -nt "socket.o" ]
+     then
+     echo "rebuilding socket library"
+     gcc -c socket.c
+fi
+
+if [ "sqlite3.c" -nt "sqlite3.o" ] || [ "sqlite3.h" -nt "sqlite3.o" ]
+	then
+	echo "rebuilding sqlite3"
+	gcc -c sqlite3.c
+fi
+
+if [ "string.c" -nt "string.o" ] || [ "string.h" -nt "string.o" ]
+	then
+	echo "rebuilding string library"
+	gcc -c string.c
+fi
+
+if [ "httpHandler.c" -nt "httpHandler.o" ] || [ "httpHandler.h" -nt "httpHandler.o" ]
+	then
+	echo "rebuilding httpHandler"
+	gcc -c httpHandler.c
+fi
+
+if [ "commentSense.c" -nt "commentSense.o" ] || [ "commentSense.h" -nt "commentSense.o" ]
+	then
+	echo "rebuilding commentSense"
+	gcc -c commentSense.c
+fi
+
+#gcc -c socket.c
+#gcc -c sqlite3.c
+#gcc -c string.c
+#gcc -c httpHandler.c
+#gcc -c commentSense.c
 
 libs='-lpthread -ldl'
 link='cJSON.o socket.c sqlite3.o string.o httpHandler.o commentSense.c'
 
 if [ "$debug" == "1" ]
     then
-    	echo "debug build"
+   	echo "debug build"
 	gcc main.c $link -o server $libs -Wall -D DEBUG
-    else
-    	echo "release build"
+else
+   	echo "release build"
 	gcc main.c $link -o server $libs -Wall
 fi
 
