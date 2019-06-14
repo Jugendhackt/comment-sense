4c4cf59594db0f266d540b35123be43339c2ac8c
Galaxyqasar <kunze.georg44@gmail.com>
2019-06-14 21:20:38 +0200

    cleanup

--- Server-Cpp/build.sh
+++ Server-Cpp/build.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
 checkRequirement () {
-    if ! dpkg -s $1 &> /dev/null
-        then
-            echo "$1 has to be installed"
-            sudo apt-get install $1
-    fi
+	if ! dpkg -s $1 &> /dev/null; then
+		echo "should the required package '$1' be installed? (Y/N)"
+		read ans
+		if [ $ans == "Y" ] || [ "$ans" == "y" ] ; then
+			sudo apt-get install $1
+		else
+			exit -1
+		fi
+	fi
 }
 
 buildCLib () {
-    if [ "$1.c" -nt "$1.o" ] || [ "$1.h" -nt "$1.o" ] || [ "$2" == "1" ]
-        then
-            echo "building lib '$1'"
-            gcc -c $1.c $3
-    fi
+	if [ "$1.c" -nt "$1.o" ] || [ "$1.h" -nt "$1.o" ] || [ "$2" == "1" ]; then
+		echo "building lib '$1'"
+		gcc -c $1.c $3
+	fi
 }
 
 buildCppLib () {
-    if [ "$1.cpp" -nt "$1.o" ] || [ "$1.hpp" -nt "$1.o" ] || [ "$2" == "1" ]
-        then
-            echo "building lib '$1'"
-            g++ -c $1.cpp $3 -std=c++11
-    fi
+	if [ "$1.cpp" -nt "$1.o" ] || [ "$1.hpp" -nt "$1.o" ] || [ "$2" == "1" ]; then
+		echo "building lib '$1'"
+		g++ -c $1.cpp $3 -std=c++11
+	fi
 }
 
 loadData () {
-    if [ -d data ]
-        then
-            rm data -r
-    fi
-    mkdir data
-    cp ../MainSite/* data/ -r
-    
-    ipAdress=($(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'))
-    echo "private ip adress is '$ipAdress'"
-    sed -i -- "s/const ipAdress = here;/const ipAdress = \"$ipAdress\"/g" ./data/JS/*.js
+	if [ -d data ]; then
+		rm data -r
+	fi
+	mkdir data
+	cp ../MainSite/* data/ -r
+
+	ipAdress=($(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'))
+	echo "private ip adress is '$ipAdress'"
+	sed -i -- "s/const ipAdress = here;/const ipAdress = \"$ipAdress\"/g" ./data/JS/*.js
 }
 
-rebuild=0
+rebuild=0, start=0
 target="debug"
 defines=""
 options=""
@@ -47,20 +48,20 @@ link='cJSON.o tcpSocket.o sqlite3.o utils.o httpServer.o commentSense.o'
 args='-std=c++11 -Wall'
 
 for i in "$@"
-    do
+	do
     	case $i in
-	    -d | --debug ) target="debug";;
-            -r | --release ) target="release";;
-            -t=* | --target=* ) target="${i#*=}";;
-            -s | --static ) libs="$libs $staticLibs";;
-            --rebuild ) rebuild=1;;
-	    * ) echo "unknown arg";;
+		-d | --debug ) target="debug";;
+		-r | --release ) target="release";;
+		-t=* | --target=* ) target="${i#*=}";;
+		-s | --static ) libs="$libs $staticLibs";;
+		-a | --start ) start=1;;
+		--rebuild ) rebuild=1;;
+		* ) echo "unknown arg ${i#*=}";;
 	esac
 	shift
 done
 
-if [ "$target" == "debug" ]
-	then
+if [ "$target" == "debug" ]; then
 	defines="-D DEBUG"
 	options="-g"
 else
@@ -86,3 +87,8 @@ echo "copying data folder"
 loadData ;
 
 echo "done"
+
+if [ "$start" == "1" ]; then
+	echo "starting server ..."
+	sudo ./server
+fi
