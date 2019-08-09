8dd87b9e0ceb508f1dabc33dc19e5e0b5e95b92d
73nici <30949443+73nici@users.noreply.github.com>
2019-08-09 20:33:47 +0200

    REMOVE *

--- reactPlugin/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./style.css";
-import "./bootstrap/bootstrap.css";
-import "./bootstrap/theme.css";
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
