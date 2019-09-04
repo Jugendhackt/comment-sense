4d1cd2ad0cf2528a518b3ecdcbd9694f8d9cfb1e
73nici <30949443+73nici@users.noreply.github.com>
2019-09-04 20:51:47 +0200

    add build

---
+++ plugin/commentsense/build/precache-manifest.f0a57c7e69a14bb6fbb61aba9a0bd097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7add2a79197d17d19c38ffae269fd76f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c27b341b13d8c5476157",
+    "url": "/static/css/main.f4e6043b.chunk.css"
+  },
+  {
+    "revision": "b1ec9f734ef265eecc69",
+    "url": "/static/js/2.269b988d.chunk.js"
+  },
+  {
+    "revision": "c27b341b13d8c5476157",
+    "url": "/static/js/main.1e7075ba.chunk.js"
+  },
+  {
+    "revision": "7387197b25349b5deae7",
+    "url": "/static/js/runtime~main.7851a691.js"
+  }
+]);
