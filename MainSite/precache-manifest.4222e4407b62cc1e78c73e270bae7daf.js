0f96278bcc296a1e68a02d7e21d1938e4839ec81
73nici <30949443+73nici@users.noreply.github.com>
2019-11-03 20:14:29 +0100

    new mainsite

---
+++ MainSite/precache-manifest.4222e4407b62cc1e78c73e270bae7daf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d0c3e8a109a421530a19beac85f062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65918d0c46e7bb8cbf1d",
+    "url": "/static/css/main.3729df9c.chunk.css"
+  },
+  {
+    "revision": "be3c8c010d5f67f9dffa",
+    "url": "/static/js/2.29d21001.chunk.js"
+  },
+  {
+    "revision": "65918d0c46e7bb8cbf1d",
+    "url": "/static/js/main.7816614a.chunk.js"
+  },
+  {
+    "revision": "ba4c316a4984c5a17fee",
+    "url": "/static/js/runtime-main.0298c362.js"
+  }
+]);
