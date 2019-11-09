ffbde1d6fc44d69b3e2a948ecec3f1ebd7d6bda6
73nici <30949443+73nici@users.noreply.github.com>
2019-11-09 20:40:19 +0100

    ADD minWidth to websites

--- main-site/src/components/TopWebsites/Website.js
+++ main-site/src/components/TopWebsites/Website.js
@@ -14,7 +14,8 @@ const useStyles = makeStyles(theme => ({
     },
     paper: {
         margin: theme.spacing(1),
-        padding: "5%"
+        padding: "5%",
+        minWidth: "40%"
     },
     text: {
         marginLeft: theme.spacing(1)
@@ -25,30 +26,29 @@ const useStyles = makeStyles(theme => ({
 function Website(props) {
     const classes = useStyles();
 
+    /*
     const showTitle = () => {
-        if (props.url.length > 20) {
-            return props.url.substring(0, 20) + "...";
+        if (props.url.length > 40) {
+            return props.url.substring(0, 40) + "...";
         }
         return props.url;
-    };
+    }; */
 
 
     return (
-        <>
-            <ListItem button className={classes.website} >
-                <Paper className={classes.paper}>
-                    <Typography variant="h5">{showTitle()}</Typography>
-                    <br />
-                    <Box display="flex">
-                        <ThumbUp color="secondary" />
-                        <ListItemText primary={props.comments} className={classes.text} />
-                    </Box>
-                    <Link color="inherit" href={props.url}>
-                        <Button color="primary" variant="contained">{langDe.toWebsite}</Button>
-                    </Link>
-                </Paper>
-            </ListItem>
-        </>
+        <ListItem button className={classes.website} >
+            <Paper className={classes.paper}>
+                <Typography variant="h5">{props.url}</Typography>
+                <br />
+                <Box display="flex">
+                    <ThumbUp color="secondary" />
+                    <ListItemText primary={props.comments} className={classes.text} />
+                </Box>
+                <Link color="inherit" href={props.url}>
+                    <Button color="primary" variant="contained">{langDe.toWebsite}</Button>
+                </Link>
+            </Paper>
+        </ListItem>
     );
 };
 
