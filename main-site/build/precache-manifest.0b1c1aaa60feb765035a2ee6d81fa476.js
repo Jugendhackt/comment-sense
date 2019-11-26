8ddde8c55d85c03a30b8dca46e917a5e59e85fba
73nici <30949443+73nici@users.noreply.github.com>
2019-11-26 16:54:12 +0100

    test

---
+++ main-site/build/precache-manifest.0b1c1aaa60feb765035a2ee6d81fa476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34597a1b6c8ef6821c69849dc67dde5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2c4f7c9f95527fe0bec",
+    "url": "./static/css/main.3729df9c.chunk.css"
+  },
+  {
+    "revision": "dc0437b79ef06b91ca06",
+    "url": "./static/js/2.1dc185e5.chunk.js"
+  },
+  {
+    "revision": "c2c4f7c9f95527fe0bec",
+    "url": "./static/js/main.0c5dbefa.chunk.js"
+  },
+  {
+    "revision": "9baa184ae00472f698c9",
+    "url": "./static/js/runtime-main.f05fecf6.js"
+  }
+]);
